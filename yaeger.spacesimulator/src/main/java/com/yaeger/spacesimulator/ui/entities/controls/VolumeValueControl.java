929c1ca1cdf5edbf67994661e858414786ee2704
Wiebe van Aken <wiebevaken@gmail.com>
2021-10-17 19:29:35 +0200

    Spul is configureerbaar

--- yaeger.spacesimulator/src/main/java/com/yaeger/spacesimulator/ui/entities/controls/VolumeValueControl.java
+++ yaeger.spacesimulator/src/main/java/com/yaeger/spacesimulator/ui/entities/controls/VolumeValueControl.java
@@ -3,11 +3,20 @@ package com.yaeger.spacesimulator.ui.entities.controls;
 import java.text.DecimalFormat;
 
 import com.github.hanyaeger.api.Coordinate2D;
+import com.yaeger.spacesimulator.services.ConfigService;
 
 public class VolumeValueControl extends ValueControl {
 
 	public VolumeValueControl(Coordinate2D initialLocation, double width) throws Exception {
-		super(initialLocation, width, "Volume", 1, 1, 1500, new DecimalFormat("###,###,###.###"));
+		super(
+			initialLocation, 
+			width, 
+			"Volume", 
+			Double.parseDouble(ConfigService.getValue("base-planet-volume")), 
+			Double.parseDouble(ConfigService.getValue("planet-min-volume")), 
+			Double.parseDouble(ConfigService.getValue("planet-max-volume")), 
+			new DecimalFormat("###,###,###.###")
+		);
 		setSubtitle("(x 10^12 km^3)");
 	}
 
